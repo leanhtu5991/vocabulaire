d552736bb0c2d481c921e8037d85e43e237365cc
nsta9x <sontn.ftu@gmail.com>
2019-04-07 21:50:43 +0200

    Commit login signup html

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 const routes: Routes = [
-  {path : '', component : MainPageComponent}
+  {path : '', component : MainPageComponent},
+  {path : 'login', component : LoginComponent},
+  {path : 'signup', component : SignupComponent},
+  {path : 'aboutus', component : AboutUsComponent}
 ];
 
 @NgModule({
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -6,15 +6,25 @@ import { DatePipe } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Metro } from './classes/metro';
 import { MenuUserComponent } from './components/menu-user/menu-user.component';
 import { MenuAdminComponent } from './components/menu-admin/menu-admin.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainPageComponent,
     MenuUserComponent,
-    MenuAdminComponent
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    MenuAdminComponent,
+    HeaderComponent,
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +33,6 @@ import { MenuAdminComponent } from './components/menu-admin/menu-admin.component
     FormsModule
   ],
   providers: [
-    Metro,
     DatePipe
   ],
   bootstrap: [AppComponent]
